510b5322c40a7cd79667db352fa993b745ecd99c
suhuna <suhuna@blueoshan.com>
2023-08-16 23:39:22 +0530

    fourth  push

--- models/implements.js
+++ models/implements.js
@@ -1,29 +1,32 @@
 const mongoose = require('mongoose')
 
 const implement= new mongoose.Schema({
-  implements:{
-  type:String,
-      required:true
+implements:{
+type:String,
+    required:true
 },
 name:{
-  type:String,
-      required:true
+type:String,
+    required:true
 },
-  function:{
-  type:String
+function:{
+type:String
 },
-  Specifications:{
-  type:String
+Specifications:{
+type:String
+},
+General_Information:{
+    type:String
+},
+Cost_of_the_unit:{
+    type:String
+},
+image_url:{
+    type:String
   },
-  General_Information:{
-      type:String
-  },
-  Cost_of_the_unit:{
-      type:String
-  },
-  image_url:{
-      type:String
-    }
+  buy_now_url:{
+    type:String
+  }
 })
 
 module.exports = mongoose.model('implement',implement)
---
+++ public/service-worker.js
@@ -0,0 +1,50 @@
+const staticCacheName = 'tam-v1';
+
+
+const filesToCache = [
+  'images/app-logo.jpg'
+];
+
+self.addEventListener('install', event => {
+  console.log('Attempting to install service worker and cache static assets');
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(staticCacheName)
+    .then(cache => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+this.addEventListener('activate', function(event) {
+
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (staticCacheName.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);  
+  if (url.origin == location.origin && ( event.request.url.indexOf('Diagnostic') > -1 || url == 'https://pkvd-agro.onrender.com/' || url == 'https://pkvd-agro.onrender.com/')) {  
+    event.respondWith(caches.open(staticCacheName).then((cache) => {
+      return cache.match(event.request).then((cachedResponse) => {
+        const fetchedResponse = fetch(event.request).then((networkResponse) => {
+          console.log("adding to cache: "+event.request.url);
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+
+        return cachedResponse || fetchedResponse;
+      });
+    }));
+  }
+  else {
+    return;
+  }  
+});
